ac39b29e7b8dad9755771f75ecbeb7263e678992
“Aariz <aariz120@gmail.com>
2024-05-21 14:50:26 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Aariz Mobin',
+    author_email= 'aariz120@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
